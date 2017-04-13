900a1c411b18c87e6a804675bbd77347d6d8b7a4
Chris Workman <cworkman@theprescientinvestor.com>
2017-04-12 23:00:59 -0700

    Adding Microservices

---
+++ MicroServices/HelloMicroservices/HelloMicroservices/CurrentDateTimeModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloMicroservices
+{
+    using Nancy;
+
+    public class CurrentDateTimeModule: NancyModule 
+    {
+        public CurrentDateTimeModule()
+        {
+            Get("/", _ => DateTime.UtcNow);
+        }
+    }
+}
